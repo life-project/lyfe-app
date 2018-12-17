e65d9011897019fa8104673abbc95acd3360fdbb
Tim Schoen <schoentr@gmail.com>
2018-12-17 13:53:38 -0800

    js added to get prompts from DOM

--- js/app.js
+++ js/app.js
@@ -1,44 +1,44 @@
-`use strict`
- var promptElement = getElementById('form');
- var newQuestionElement = getElementById('question');
- var newButton1Element;
- var newButton2Element;
- var questionIndicatior = 0;
- var question = ["Do you want to go to school",
-  
- ]
+`use strict`;
+var promptElement = document.getElementById('form');
+// var newQuestionElement = document.getElementById('question').textContent;
+var newButton1Element = document.getElementById('input1');
+var newButton2Element = document.getElementById('input2');
+var questionIndicator = 0;
+
+var question = {
+  prompt:'Do you want to go to school',
+  button1:'button1',
+  button2:'button2',
+};
+
 var player = {
-  name: tim,
+  name: 'tim',
   age: 18,
   job: null,
   salary:null,
   savings:10000,
 
+};
 
-
-}
-
-function question(){
-  newLabelElement = question[questionIndicatior];
-  newLabelElement.appendChild(promptElement);   
+function questionFunc(){
+   document.getElementById('question').textContent = question.prompt;
 }
 function randomEvent(){
 
 }
 
 function payday(){
-  savings +=salary;
+  player.savings += player.salary;
 }
 
 
-function startChoice(){
-  alert('yes');
-  if {
-  }else{
-
-  }
-
+function startChoice1(){
+  alert('1');
 }
+function startChoice2(){
+  alert('2');
+}
+questionFunc();
 
-promptElement.addEventListener("click",startChoice);
-question();
+newButton1Element.addEventListener('click',startChoice1);
+newButton2Element.addEventListener('click',startChoice2);
