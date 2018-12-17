6ac1413b3f9e66b83332ffc3d3f4e97d04b84b89
Tim Schoen <schoentr@gmail.com>
2018-12-17 13:10:26 -0800

    started js file

---
+++ js/app.js
@@ -0,0 +1,44 @@
+`use strict`
+ var promptElement = getElementById('form');
+ var newQuestionElement = getElementById('question');
+ var newButton1Element;
+ var newButton2Element;
+ var questionIndicatior = 0;
+ var question = ["Do you want to go to school",
+  
+ ]
+var player = {
+  name: tim,
+  age: 18,
+  job: null,
+  salary:null,
+  savings:10000,
+
+
+
+}
+
+function question(){
+  newLabelElement = question[questionIndicatior];
+  newLabelElement.appendChild(promptElement);   
+}
+function randomEvent(){
+
+}
+
+function payday(){
+  savings +=salary;
+}
+
+
+function startChoice(){
+  alert('yes');
+  if {
+  }else{
+
+  }
+
+}
+
+promptElement.addEventListener("click",startChoice);
+question();
