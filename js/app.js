4e0da903bc774a87c75ff39c5961ded2d5e3a100
Tim Schoen <schoentr@gmail.com>
2018-12-18 22:40:13 -0800

    worked on pic logic

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ var nameElement = document.getElementById('name');
 var carImageElement = document.getElementById('second-ti');
 var educationImageElement = document.getElementById('first-ti');
 var kidImageElement = document.getElementById('third-ti');
-
+var randomIndex;
 var newLiabilitiesElement = document.getElementById('liabilities');
 var button3Flag=false;
 
@@ -28,7 +28,7 @@ Question.allQuestions= [];
 new Question('Do you want to go to school?','You will need a loan of $80,000', 'Yes','No');
 new Question('Do you want to by a car?','','Yes','No');
 new Question('What type of car would you like','' ,'A new car', 'A used Car');
-new Question('Do you want to get married?', '', 'Yes','No');
+new Question('Do you want to get married?', 'Two incomes are better then one.', 'Yes','No');
 new Question('Do you want to buy a house?','House cost a lot of money', 'Yes I don\'t want to rent forever', 'No I prefer wasting my money');
 new Question('How do you feel about kids', ' ', 'I want one now', 'Not for me');
 
@@ -42,9 +42,7 @@ var randomEventPrompt = [['You won the Lotto','You gained 50,000... PLAY THE MEG
 
 
 
-function randomNumber(){
-  return Math.floor(Math.random()*(randomEventPrompt.length));
-}
+
 
 var Player = {
   name:nameElement,
@@ -55,11 +53,11 @@ var Player = {
   savings :10000,
   car:false,
   newcar:false,
+  kids:false,
+  kidsNumber:0,
 };
 
 function questionFunc(){
-  // alert('question func');
-  console.log(i);
   showQuestions();
   document.getElementById('question').textContent = Question.allQuestions[i].prompt;
   document.getElementById('description').textContent = Question.allQuestions[i].description;
@@ -69,8 +67,39 @@ function questionFunc(){
 
 }
 
+function randomNumber(){
+  return Math.floor(Math.random()*(randomEventPrompt.length));
+}
+
+function randomLogic(){
+  if (randomIndex===0) {
+    Player.savings+=50000;
+  }
+  if (randomIndex===1){
+    var liElement =document.createElement('li');
+    liElement.textContent = 'Kid: $25,000';
+    newLiabilitiesElement.appendChild(liElement);
+    Player.kids=true;
+    Player.kidsNumber++;
+  }
+  if (randomIndex===2){
+    Player.savings+=40000;
+  }
+  if (randomIndex===3){
+    Player.savings-=35000;
+  }
+  if (randomIndex===4){
+    Player.savings+=10000;
+  }
+  if (randomIndex===5){
+    Player.salary+=5000;
+  }
+
+}
+
 function randomEventRender(){
-  var randomIndex = randomNumber();
+  randomIndex = randomNumber();
+  randomLogic();
   showEvents();
   document.getElementById('event').textContent = randomEventPrompt[randomIndex][0];
   document.getElementById('event-description').textContent = randomEventPrompt[randomIndex][1];
@@ -85,32 +114,24 @@ function updateStatus(){
   document.getElementById('money-status').textContent = ('Savings: $' + Player.savings);
 }
 
-function liabilitiesFunc(){
-  if(i===0){
-    var liElement =document.createElement('li');
-    liElement.textContent = 'Student Loans: $80,000';
-    newLiabilitiesElement.appendChild(liElement);
-  }
-}
+// function liabilitiesFunc(){
+//   if(i===0){
+//     var liElement =document.createElement('li');
+//     liElement.textContent = 'Student Loans: $80,000';
+//     newLiabilitiesElement.appendChild(liElement);
+//   }
+// }
 
 function pictureLogic(){
   if(i===0){
-    if (Player.education===true) educationImageElement.src='images/graduation.png';
-    if (Player.education===false) educationImageElement.src='images/graduationx.png';
+    if (Player.education) educationImageElement.src='images/graduation.png';
+    if (!Player.education) educationImageElement.src='images/graduationx.png';
   }
-  if(i===1){
-    if(Player.car===false){
-      carImageElement.src= 'images/newcarx.png';
-    }
-    if (i===2){
-      if(Player.newcar===true)carImageElement.src ='images/newcar.png';
-      if(Player.newcar===false)carImageElement.src = 'images/oldcar.png';
-    }
-    
-
-
+  if(i>=1){
+    if(!Player.car)carImageElement.src= 'images/newcarx.png';
+    if(Player.car && !Player.newcar)carImageElement.src ='images/oldcar.png';
+    if(Player.car && Player.newcar)carImageElement.src ='images/newcar.png';
   }
-
 }
 
 function showEvents (){
@@ -142,12 +163,13 @@ function payday(){
 
 }
 function logic(){
-
   if (i===0){
     Player.age+=4;
     if(choice===1){
       Player.education=true;
-      liabilitiesFunc();
+      var liElement =document.createElement('li');
+      liElement.textContent = 'Student Loans: $80,000';
+      newLiabilitiesElement.appendChild(liElement);
     }
     else{
       for(var j=0; j<4;j++){
@@ -161,36 +183,37 @@ function logic(){
   if (i===2){
     if(choice===1){
       Player.newcar=true;
-   }
+    }
   }
 
-  pictureLogic();
+  if(i!==1)pictureLogic();
+  if(i===1 && Player.car===false)pictureLogic();
 }
-function startChoice1(event){
+function startChoice1(event){ 
   choice=1;
-  logic();
-
-  if(i===1){
-    // alert(i);
+   if(i===1){
     i++;
     questionFunc();
-  } else{
+  }else{
+    logic();
     randomEventRender();
+    i++;
   }
-  i++;
 
 }
 
 function startChoice2(event){
   choice=2;
   logic();
-  i++;
+  if(i===1)i++;
   randomEventRender();
+  i++;
+
 }
 
 function startChoice3(event){
   console.log(button3Flag);
-  if(button3Flag===false){
+  if(!button3Flag){
     payday();
     button3Flag=true;
   }else{
@@ -201,9 +224,9 @@ function startChoice3(event){
 
 checkforName();
 // showEvents();
-randomEventRender();
+// randomEventRender();
 questionFunc();
-// i++;
+
 // questionFunc();
 
 newButton1Element.addEventListener('click',startChoice1);
